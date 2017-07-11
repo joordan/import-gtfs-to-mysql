02ce7f8aa23eec8eb777d69adbd0cb241930917d
Jordan <jordanespejo@gmail.com>
2017-07-11 11:59:28 -0700

    Update import-gtfs-mysql.sh

--- import-gtfs-mysql.sh
+++ import-gtfs-mysql.sh
@@ -3,4 +3,4 @@ echo enter database name:
 read dbname
 echo enter database username:
 read username
-(cat creator2.sql | mysql -u $username $dbname -p --local-infile && echo import successful) || echo import failed
+(cat definition.sql | mysql -u $username $dbname -p --local-infile && echo import successful) || echo import failed
